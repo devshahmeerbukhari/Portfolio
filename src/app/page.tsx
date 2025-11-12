775bc224fd5b45e724d09a9a381403763168040f
devshahmeerbukhari <shahmeerbukhari13@gmail.com>
2025-11-13 03:31:49 +0500

    Error Fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -34,7 +34,6 @@ export default function Home() {
     return () => clearTimeout(intervalId); // Clean up the timeout on unmount
   }, [currentDisplay, professions]);
 
-  
   return (
     <>
       <style jsx>{`
